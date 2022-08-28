c572a47455ca0fb13a1ba154998e52366b2c63fc
ac0mz <aco.drumming@gmail.com>
2022-08-29 05:07:30 +0900

    add: login service

--- entity/user.go
+++ entity/user.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type UserID int64
 
@@ -11,4 +15,10 @@ type User struct {
 	Role     string    `json:"role" db:"role"`
 	Created  time.Time `json:"created" db:"created"`
 	Modified time.Time `json:"modified" db:"modified"`
+}
+
+// ComparePassword はハッシュ化されて永続化されたパスワードを入力値のパスワードと比較検証する。
+// 一致している場合はnil、不一致の場合はerrorを返却する。
+func (u *User) ComparePassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
 }
--- service/interface.go
+++ service/interface.go
@@ -9,7 +9,7 @@ import (
 
 // 以下インターフェースはstore/task.goに実装する
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . TaskLister TaskAdder UserRegister
+//go:generate go run github.com/matryer/moq -out moq_test.go . TaskLister TaskAdder UserRegister UserGetter TokenGenerator
 type TaskLister interface {
 	ListTasks(ctx context.Context, db store.Queryer) (entity.Tasks, error)
 }
@@ -20,4 +20,12 @@ type TaskAdder interface {
 
 type UserRegister interface {
 	RegisterUser(ctx context.Context, db store.Execer, u *entity.User) error
+}
+
+type UserGetter interface {
+	GetUser(ctx context.Context, db store.Queryer, name string) (*entity.User, error)
+}
+
+type TokenGenerator interface {
+	GenerateToken(ctx context.Context, u entity.User) ([]byte, error)
 }
