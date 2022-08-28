3e2ea3f608a3111d2b8dfcf03ff3921f7c01131e
ac0mz <aco.drumming@gmail.com>
2022-08-28 16:39:34 +0900

    add: generate token

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"time"
 
 	"github.com/ac0mz/go_todo_app/clock"
 	"github.com/ac0mz/go_todo_app/entity"
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 )
 
 //go:embed cert/secret.pem
@@ -30,7 +34,7 @@ type Store interface {
 }
 
 // NewJWTer はPEMキーの解析と構造体の初期化を行う
-func NewJWTer(s Store) (*JWTer, error) {
+func NewJWTer(s Store, c clock.Clocker) (*JWTer, error) {
 	privKey, err := parse(rawPrivKey)
 	if err != nil {
 		return nil, fmt.Errorf("failed in NewJWTer: private key: %w", err)
@@ -44,7 +48,7 @@ func NewJWTer(s Store) (*JWTer, error) {
 		Store:      s,
 		PrivateKey: privKey,
 		PublicKey:  pubKey,
-		Clocker:    clock.RealClocker{},
+		Clocker:    c,
 	}
 	return j, nil
 }
@@ -56,4 +60,38 @@ func parse(rawKey []byte) (jwk.Key, error) {
 		return nil, err
 	}
 	return key, nil
+}
+
+const (
+	RoleKey     = "role"
+	UserNameKey = "user_name"
+)
+
+// GenerateToken はユーザ情報と秘密鍵を元にJWTトークンを生成する。
+// また、トークン生成時に作成したUUID（JWT ID）をキーにRedisへユーザIDを登録する。
+func (j JWTer) GenerateToken(ctx context.Context, u entity.User) ([]byte, error) {
+	token, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/ac0mz/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(j.Clocker.Now()).
+		Expiration(j.Clocker.Now().Add(30*time.Minute)).
+		Claim(RoleKey, u.Role).     // 独自クレーム(ロール)
+		Claim(UserNameKey, u.Name). // 独自クレーム(ユーザ名)
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("GetToken: failed to build token: %w", err)
+	}
+
+	// UUIDをキーにユーザIDを登録
+	if err := j.Store.Save(ctx, token.JwtID(), u.ID); err != nil {
+		return nil, err
+	}
+
+	// 秘密鍵による署名を付与したJWTトークンの生成
+	signed, err := jwt.Sign(token, jwt.WithKey(jwa.RS256, j.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+	return signed, nil
 }
--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -2,7 +2,12 @@ package auth
 
 import (
 	"bytes"
+	"context"
 	"testing"
+
+	"github.com/ac0mz/go_todo_app/clock"
+	"github.com/ac0mz/go_todo_app/entity"
+	"github.com/ac0mz/go_todo_app/testutil/fixture"
 )
 
 func TestEmbed(t *testing.T) {
@@ -24,5 +29,33 @@ func TestEmbed(t *testing.T) {
 	wantEnd = []byte("-----END PRIVATE KEY-----")
 	if !bytes.Contains(rawPrivKey, wantEnd) {
 		t.Errorf("want %s, but got %s", wantEnd, rawPrivKey)
+	}
+}
+
+func TestJWTer_GenerateToken(t *testing.T) {
+	ctx := context.Background()
+	wantID := entity.UserID(20)
+	u := fixture.User(&entity.User{ID: wantID})
+
+	moq := &StoreMock{}
+	moq.SaveFunc = func(ctx context.Context, key string, userID entity.UserID) error {
+		if userID != wantID {
+			t.Errorf("want %d, but got %d", wantID, userID)
+		}
+		return nil
+	}
+
+	sut, err := NewJWTer(moq, clock.FixedClocker{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 実行と検証
+	got, err := sut.GenerateToken(ctx, *u)
+	if err != nil {
+		t.Errorf("not want err: %v", err)
+	}
+	if len(got) == 0 {
+		t.Errorf("got token is empty")
 	}
 }
